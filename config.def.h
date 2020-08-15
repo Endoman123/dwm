6c421bcbe170d6d5a02f5d7591b2e4aaf46bef6f
Jared Tulayan <jared@jaredtulayan.xyz>
2020-08-15 12:44:31 -0400

    Add window rules

--- config.def.h
+++ config.def.h
@@ -36,7 +36,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Brave-browser",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "TeamViewer",     "TeamViewer",       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
